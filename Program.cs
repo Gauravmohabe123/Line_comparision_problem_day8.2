c14d6bec105e4de257e36f9828d898dd99b2c590
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-02 14:15:30 +0530

    Added Line_comp_Prob

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comp_Prob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
