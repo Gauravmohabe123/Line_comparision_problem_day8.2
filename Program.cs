7a1ad3a532b34e34278b6b03dfb08e05983a857e
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-02 14:42:38 +0530

    Added: Line_comparision_problem

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers.Text;
 
 namespace Line_Comp_Prob
 {
@@ -6,7 +7,30 @@ namespace Line_Comp_Prob
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            double obj = Program.line();
+             
         }
+        static double line()
+        {
+            Console.WriteLine("Enter the first two numburs for x1 and y1");
+            Console.WriteLine("Enter x1");
+            double x1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            double y1=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first two numburs for x2 and y2");
+            Console.WriteLine("Enter x2");
+            double x2=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            double y2=int.Parse(Console.ReadLine());
+
+            double LenghtofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(LenghtofLine);
+            return LenghtofLine;
+        }
+        
+        
     }
 }
